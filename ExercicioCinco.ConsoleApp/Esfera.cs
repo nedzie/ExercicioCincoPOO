29a2f1e6a3ea2b133c504d379c3de7deea34e4b5
nedzie <marcos-elivelton@hotmail.com>
2022-03-08 15:41:37 -0300

    Add project files.

---
+++ ExercicioCinco.ConsoleApp/Esfera.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioCinco.ConsoleApp
+{
+    internal class Esfera
+    {
+        public double raio;
+        public double volume;
+        const double pi = 3.14;
+
+        public double CalcularVolume(double raio)
+        {
+            volume = (4 * pi * (raio * raio * raio)) / 3;
+            return volume;
+        }
+    }
+}
---
+++ ExercicioCinco.ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ExercicioCinco.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Esfera bola = new Esfera();
+            Console.Write("Informe o raio da esfera: \n> ");
+            bola.raio = double.Parse(Console.ReadLine());
+
+            bola.volume = bola.CalcularVolume(bola.raio);
+
+            Console.WriteLine("O volume da esfera é de " + bola.volume.ToString("#.##") + "m³.");
+        }
+    }
+}
